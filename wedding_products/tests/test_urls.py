112d4814509d8e4dc1383879d8c08cddcfdb433e
Łukasz <boclukasz@gmail.com>
2020-05-04 22:39:35 +0200

    test_add_category_url

--- wedding_products/tests/test_urls.py
+++ wedding_products/tests/test_urls.py
@@ -45,6 +45,20 @@ class TestUrls:
                                      'realization_date':'2020-08-10', 'product':product, 'visit':visit})
         assert response.status_code == 302
 
+    @pytest.mark.django_db
+    def test_add_category_url(self):
+        c = Client()
+        c.login(username='Testowy', password='Testowe123')
+        visit = Visit.objects.create(first_name='Natalia', last_name='Nowa', telephone_number='888333777',
+                                     visit_date='2011-09-01T13:20:30+03:00')
+        category = Category.objects.create(name='Suknie ślubne')
+        product = Product.objects.create(product_name='Suknia-testowa', description='super',
+                                         cost_of_production='1500.00',
+                                         selling_net_price='2899', category=category, quantity='3')
+        response = c.post('/add_order/', {'height':'155', 'waist':'70', 'hips':'90', 'breast':'90',
+                                 'additional_info':'No info', 'order_date':'2020-05-05',
+                                 'realization_date':'2020-08-10', 'product':product, 'visit':visit})
 
+        assert response.status_code == 302
 
 
