b4f2172930c8b2ab986cb93c1aa35349ff3721c0
Łukasz <boclukasz@gmail.com>
2020-05-25 23:29:14 +0200

    Displying visits in calendar

--- wedding_products/static/js/app.js
+++ wedding_products/static/js/app.js
@@ -1,6 +1,42 @@
 
+document.addEventListener('DOMContentLoaded', function () {
+
+
 $(function(){
 
     $('#logo').fadeIn(3000);
 
 });
+
+ const visitsRows = document.querySelectorAll('.client_data');
+ visitsRows.forEach(function (visitRow) {
+
+
+     const calendarDates = document.querySelectorAll('.fc-content-skeleton td');
+     calendarDates.forEach(function (calendarDate) {
+     if (calendarDate.dataset.date === visitRow.dataset.date) {
+         if (calendarDate.classList.contains('fc-fri')) {
+             const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[4];
+             myCell.innerHTML = visitRow.dataset.first_name + " " + visitRow.dataset.hour;
+         } else if (calendarDate.classList.contains('fc-mon')) {
+             const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[0];
+             myCell.innerHTML = visitRow.dataset.first_name + " " + visitRow.dataset.hour;
+         } else if (calendarDate.classList.contains('fc-tue')) {
+             const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[1];
+             myCell.innerHTML = visitRow.dataset.first_name + " " + visitRow.dataset.hour;
+         } else if (calendarDate.classList.contains('fc-wed')) {
+             const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[2];
+             myCell.innerHTML = visitRow.dataset.first_name + " " + visitRow.dataset.hour;
+         } else if (calendarDate.classList.contains('fc-thu')) {
+             const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[3];
+             myCell.innerHTML = visitRow.dataset.first_name + " " + visitRow.dataset.hour;
+         }
+     }
+ });
+
+
+ })
+
+
+
+    });
