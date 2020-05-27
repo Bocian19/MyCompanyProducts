0b4862892d03a6126c031682ea65867e2fa87594
Łukasz <boclukasz@gmail.com>
2020-05-27 20:20:43 +0200

    Calendar updated

--- wedding_products/static/js/app.js
+++ wedding_products/static/js/app.js
@@ -1,42 +1,44 @@
+document.addEventListener('DOMContentLoaded', function go() {
 
-document.addEventListener('DOMContentLoaded', function () {
+    $(function () {
+        $('#logo').fadeIn(3000);
+    });
+
+    const visitsRows = document.querySelectorAll('.client_data');
+    visitsRows.forEach(function start(visitRow) {
+
+        const calendarDates = document.querySelectorAll('.fc-content-skeleton td');
 
 
-$(function(){
+            calendarDates.forEach(function showVisits(calendarDate) {
+                if (calendarDate.dataset.date === visitRow.dataset.date) {
+                    if (calendarDate.classList.contains('fc-fri')) {
+                        const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[4];
+                        myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour + "</br>";
+                    } else if (calendarDate.classList.contains('fc-mon')) {
+                        const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[0];
+                        myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour;
+                    } else if (calendarDate.classList.contains('fc-tue')) {
+                        const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[1];
+                        myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour;
+                    } else if (calendarDate.classList.contains('fc-wed')) {
+                        const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[2];
+                        myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour;
+                    } else if (calendarDate.classList.contains('fc-thu')) {
+                        const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[3];
+                        myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour;
+                    }
+                }
+            });
 
-    $('#logo').fadeIn(3000);
+    })
+    const changeMonthButton = document.querySelectorAll('.fc-right button');
+
+    changeMonthButton.forEach(button => {
+        button.addEventListener('click', function () {
+            go();
+        })
+    })
 
 });
 
- const visitsRows = document.querySelectorAll('.client_data');
- visitsRows.forEach(function (visitRow) {
-
-
-     const calendarDates = document.querySelectorAll('.fc-content-skeleton td');
-     calendarDates.forEach(function (calendarDate) {
-     if (calendarDate.dataset.date === visitRow.dataset.date) {
-         if (calendarDate.classList.contains('fc-fri')) {
-             const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[4];
-             myCell.innerHTML = visitRow.dataset.first_name + " " + visitRow.dataset.hour;
-         } else if (calendarDate.classList.contains('fc-mon')) {
-             const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[0];
-             myCell.innerHTML = visitRow.dataset.first_name + " " + visitRow.dataset.hour;
-         } else if (calendarDate.classList.contains('fc-tue')) {
-             const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[1];
-             myCell.innerHTML = visitRow.dataset.first_name + " " + visitRow.dataset.hour;
-         } else if (calendarDate.classList.contains('fc-wed')) {
-             const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[2];
-             myCell.innerHTML = visitRow.dataset.first_name + " " + visitRow.dataset.hour;
-         } else if (calendarDate.classList.contains('fc-thu')) {
-             const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[3];
-             myCell.innerHTML = visitRow.dataset.first_name + " " + visitRow.dataset.hour;
-         }
-     }
- });
-
-
- })
-
-
-
-    });
