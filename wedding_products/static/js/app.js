7b174a44dd2f96bdbee2af294eab36620bd6fafc
Łukasz <boclukasz@gmail.com>
2020-06-01 18:17:46 +0200

    Clearence of js code

--- wedding_products/static/js/app.js
+++ wedding_products/static/js/app.js
@@ -9,27 +9,26 @@ document.addEventListener('DOMContentLoaded', function go() {
 
         const calendarDates = document.querySelectorAll('.fc-content-skeleton td');
 
-
-            calendarDates.forEach(function showVisits(calendarDate) {
-                if (calendarDate.dataset.date === visitRow.dataset.date) {
-                    if (calendarDate.classList.contains('fc-fri')) {
-                        const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[4];
-                        myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour + "</br>";
-                    } else if (calendarDate.classList.contains('fc-mon')) {
-                        const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[0];
-                        myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour + "</br>";
-                    } else if (calendarDate.classList.contains('fc-tue')) {
-                        const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[1];
-                        myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour + "</br>";
-                    } else if (calendarDate.classList.contains('fc-wed')) {
-                        const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[2];
-                        myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour + "</br>";
-                    } else if (calendarDate.classList.contains('fc-thu')) {
-                        const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[3];
-                        myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour + "</br>";
-                    }
+        calendarDates.forEach(function showVisits(calendarDate) {
+            if (calendarDate.dataset.date === visitRow.dataset.date) {
+                if (calendarDate.classList.contains('fc-fri')) {
+                    const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[4];
+                    myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour + "</br>";
+                } else if (calendarDate.classList.contains('fc-mon')) {
+                    const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[0];
+                    myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour + "</br>";
+                } else if (calendarDate.classList.contains('fc-tue')) {
+                    const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[1];
+                    myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour + "</br>";
+                } else if (calendarDate.classList.contains('fc-wed')) {
+                    const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[2];
+                    myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour + "</br>";
+                } else if (calendarDate.classList.contains('fc-thu')) {
+                    const myCell = calendarDate.parentElement.parentElement.nextElementSibling.firstChild.childNodes[3];
+                    myCell.innerHTML += visitRow.dataset.first_name + " " + visitRow.dataset.hour + "</br>";
                 }
-            });
+            }
+        });
 
     })
     const changeMonthButton = document.querySelectorAll('.fc-right button');
@@ -40,19 +39,17 @@ document.addEventListener('DOMContentLoaded', function go() {
         })
     })
     const tableRows = document.getElementById('table_rows ');
- const unhideButton = document.getElementById('hide');
+    const unhideButton = document.getElementById('hide');
     unhideButton.addEventListener('click', function (e) {
-        // e.preventDefault()
+
         tableRows.classList.toggle('hidden');
-        if (unhideButton.innerHTML === "Rozwiń tabelę") {
-            unhideButton.innerHTML = "Zwiń tabelę";
-        } else {
-            unhideButton.innerHTML = "Rozwiń tabelę"
-        }
-    })
 
-    const calendarBody = document.getElementsByClassName('fc-body');
-    console.log(calendarBody[0]);
-    calendarBody[0].classList.add('calendarStyle');
-});
-
+         if (unhideButton.innerHTML === "Rozwiń tabelę") {
+                unhideButton.innerHTML = "Zwiń tabelę";
+            } else {
+                unhideButton.innerHTML = "Rozwiń tabelę"
+            }
+        const calendarBody = document.getElementsByClassName('fc-body');
+        calendarBody[0].classList.add('calendarStyle');
+    });
+})
